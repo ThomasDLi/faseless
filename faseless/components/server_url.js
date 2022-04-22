0312f131b605d2c3cc0123942a74bdb6f38460f7
Thomas Li <77999105+ThomasDLi@users.noreply.github.com>
2022-04-22 12:52:21 -0700

    Update server_url.js

--- faseless/components/server_url.js
+++ faseless/components/server_url.js
@@ -1,6 +1,6 @@
 function server_url(){
 
-    return("https://FaselessServer.daffythomasli.repl.co")
+    return("https://FaselessServer.daffythomasli.repl.co/")
     //return("http://ec2-54-188-200-163.us-west-2.compute.amazonaws.com:8000/")
 
 }
