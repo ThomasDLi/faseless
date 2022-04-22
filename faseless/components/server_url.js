fda157da1eda103b8ae6c3470da2fd718561e30a
Thomas Li <77999105+ThomasDLi@users.noreply.github.com>
2022-04-22 12:32:43 -0700

    Update server_url.js

--- faseless/components/server_url.js
+++ faseless/components/server_url.js
@@ -1,6 +1,6 @@
 function server_url(){
 
-    return("http://127.0.0.1:8000/")
+    return("https://FaselessServer.daffythomasli.repl.co")
     //return("http://ec2-54-188-200-163.us-west-2.compute.amazonaws.com:8000/")
 
 }
